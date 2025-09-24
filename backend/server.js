4705382181a7a28ab21458097283fd8b5ae4698f
Deepakkumarsah25 <deepak232a@gmail.com>
2025-09-25 01:08:24 +1000

    add new files

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+// (Optional) for JSON request bodies
+app.use(bodyParser.json());
+
+// If you need __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"views"))
+app.use(express.static(path.join(__dirname,"public")))
+
+
+
+app.get('/', (req, res) => {
+  res.render("indexes.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+
+
